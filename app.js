6e4935a133c45ce951b096a34e5ec4e246a16567
gabrielmj23 <gabrielmj2312@gmail.com>
2022-03-14 15:47:36 -0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,48 @@
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const helmet = require('helmet');
+const cors = require('cors');
+require('dotenv').config();
+
+// Set up database connection through mongoose
+const mongoose = require('mongoose');
+const mongoDB = process.env.MONGO_URI;
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "MongoDB connection error"));
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+app.use(helmet());
+app.use(cors())
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// Catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// Error handler
+app.use(function(err, req, res, next) {
+  // Set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // Send error message
+  res.status(err.status || 500);
+  res.json("404 NOT FOUND");
+});
+
+module.exports = app;
